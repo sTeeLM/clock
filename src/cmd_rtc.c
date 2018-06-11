81595ddd3535f6d893d0ce7a1913c7992c6ddef4
sTeeLM <steel.mental@gmail.com>
2018-06-11 16:21:05 +0800

    add cmd_rtc

---
+++ src/cmd_rtc.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "stdlib.h"
+#include "cmd_rtc.h"
+#include "shell.h"
+#include "debug.h"
+#include "rtc.h"
+
+char cmd_rtc(char arg1, char arg2)
+{
+  if(arg1 == 0 && arg2 == 0) {
+    rtc_dump();
+    return 0;
+  }    
+  return 1;
+}
---
+++ src/cmd_rtc.h
@@ -0,0 +1,6 @@
+#ifndef __CLOCK_CMD_RTC_H__
+#define __CLOCK_CMD_RTC_H__
+
+char cmd_rtc(char arg1, char arg2);
+
+#endif
